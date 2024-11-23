af878c2e004ad1f8567c1c85f4ec1cc24ca79457
amurga32 <amurga32@gmail.com>
2024-11-23 12:35:57 -0300

    Actualización en componentes y archivos base

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';  // Envolvemos aquí
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/login">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
